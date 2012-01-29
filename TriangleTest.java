441267e930fc57d17c8755388ad1be256292112e
carlosandrade <carlosviansi@gmail.com>
2012-01-29 13:34:30 -0500

    Creating basic Triangle Test class. Initial tests should fail.

---
+++ TriangleTest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class TriangleTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
