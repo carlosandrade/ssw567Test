15345b518fbbf1ed4cba60e6e7a1461a6b894569
Chandler <chandlersun.moon@gmail.com>
2012-01-29 21:50:54 -0500

    Several lines in triangle program are modified to become more testable. Several bugs cleared. Testing use case by JUnit established. Testing data modified.

--- YeTriangle.java
+++ YeTriangle.java
@@ -1,51 +1,63 @@
-import java.util.*;
+import java.io.*;
 
 public class YeTriangle {
  private double a;
  private double b;
  private double c;
 
-public Triangle(double a, double b, double c) {
+public YeTriangle(double a, double b, double c) {
   this.a = a;
   this.b = b;
   this.c = c;
 }
 
  public static void main(String[] args) {
-  
-  Scanner scan = new Scanner(System.in);
-  System.out.println("first side:");
-  double a = scan.nextDouble();
-  System.out.println("second side:");
-  double b = scan.nextDouble();
-  System.out.println("third side:");
-  double c = scan.nextDouble();
-   scan.close();
-  
-  Triangle.isTriangle(a, b, c);
+ String[] inputMessage=new String[]{"Please input the 1st side.","Please input the 2nd side.","Please input the 3rd side."};
+ String[] inputData=new String[3];
+ try{
+for(int i=0;i<3;i++){
+	System.out.println(inputMessage[i]);
+	inputData[i]=Keyboard.getString();
+}
+ }catch(Exception e){
+	 e.printStackTrace();
+ }
+ System.out.println(YeTriangle.isTriangle(inputData[0], inputData[1], inputData[2]));
+
  }
 
- public static void isTriangle(double a, double b, double c) {
-
+ public static String isTriangle(String side1, String side2, String side3) {
+	 try{
+double a=Double.parseDouble(side1);
+double b=Double.parseDouble(side2);
+double c=Double.parseDouble(side3);
   if(a<=0||b<=0||c<=0)
-    System.out.println("This is not a triangle");
+    return "Not a triangle.";
 
   if(a+b>c&&a-b<c&&b-a<c) 
    {
-     if (a=b||a==c||b==c)
+     if (a==b||a==c||b==c)
       {
         if(a==b&&a==c) {
-          System.out.println("This is an equilateral");
-	return;}
+          return("Equilateral.");
+	}
         else {
-           System.out.println("This is an isosceles");
-	return;}
+           return("Isosceles.");
+	}
       }
-     System.out.println("This is a scalene");
-return;
+     return("Scalene.");
    }
 
-  System.out.println("This is not a triangle");
+  return("Not a triangle.");
+	 }catch(Exception e){
+		 return "Invalid input.";
+	 }
  }
+}
 
-
+class Keyboard{
+	public static String getString() throws IOException{
+		BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+		return reader.readLine();
+	}
+}
